7bb9448ee3d24628e31cd49839162aa44b9f6957
Brian Duff <cairndubh@gmail.com>
2020-04-14 01:31:40 -0700

    Messing around with closures

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+struct Request {
+    r: String,
+}
+
+struct Simple {
+    s: String,
+}
+
+impl Simple {
+    fn new() -> Simple {
+        Simple {
+            s: String::from("Hello"),
+        }
+    }
+
+    fn method(&self) {
+        let another = Another::new();
+        another.closure_eater(|req| self.say_hello(req));
+    }
+
+    fn say_hello(&self, req: Request) {
+        println!("Hello {} {}!", self.s, req.r);
+    }
+}
+
+struct Another {}
+
+impl Another {
+    fn new() -> Another {
+        Another {}
+    }
+
+    /// A random function that takes and immediately calls a closure
+    fn closure_eater<F>(&self, fun: F)
+    where
+        F: Fn(Request),
+    {
+        fun(Request {
+            r: String::from("OK"),
+        });
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let simple = Simple::new();
+    simple.method();
 }
