eeb99a33dc0cb94a4c7c1d6e1729f47d52b02fc0
Brian Duff <cairndubh@gmail.com>
2020-04-14 01:47:29 -0700

    Add links to possible solutions

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,13 @@ impl Simple {
 
     fn method(&self) {
         let another = Another::new();
+        // Some references for why:
+        // https://users.rust-lang.org/t/why-does-thread-spawn-need-static-lifetime-for-generic-bounds/4541
+        // https://stackoverflow.com/questions/32981382/thread-references-require-static-lifetime
+        // https://stackoverflow.com/questions/52437174/how-can-i-send-non-static-data-to-a-thread-in-rust-and-is-it-needed-in-this-exam
+        // https://stackoverflow.com/questions/53892938/rust-lifetime-issue-with-threads
+
+        // I think Arc<Mutex<>> might be the answer, the function doesn't need the whole of Simpe, it just needs one of its fields.
         another.closure_eater(|req| self.say_hello(req));
     }
 
